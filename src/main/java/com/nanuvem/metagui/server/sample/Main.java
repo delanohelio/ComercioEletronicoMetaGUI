02c632090df469482e6decabfc100f38ce597f08
Delano Oliveira <delanohelio@gmail.com>
2015-06-07 23:44:45 -0300

    Adding new widget

--- src/main/java/com/nanuvem/metagui/server/sample/Main.java
+++ src/main/java/com/nanuvem/metagui/server/sample/Main.java
@@ -1,7 +1,10 @@
 package com.nanuvem.metagui.server.sample;
 
 import com.nanuvem.metagui.server.MetaGuiEntryPoint;
+import com.nanuvem.metagui.server.api.Widget;
+import com.nanuvem.metagui.server.api.WidgetType;
 import com.nanuvem.metagui.server.container.DomainModelContainer;
+import com.nanuvem.metagui.server.controller.PropertyTypeType;
 import com.nanuvem.metagui.server.sample.entities.Cliente;
 import com.nanuvem.metagui.server.sample.entities.Fornecedor;
 import com.nanuvem.metagui.server.sample.entities.ItemPedido;
@@ -13,6 +16,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		MetaGuiEntryPoint.run(args);
+		Widget datePickerWidget = MetaGuiEntryPoint.createWidget("DatePickerWidget", WidgetType.Property, MetaGuiEntryPoint.readWidgetFile("DatePickerWidget.js"));
+		MetaGuiEntryPoint.createRule("field", null, PropertyTypeType.date, null, datePickerWidget, null);
 		DomainModelContainer.deploy(Cliente.class, Fornecedor.class,
 				ItemPedido.class, Pedido.class, Produto.class, Vendedor.class);
 	}
