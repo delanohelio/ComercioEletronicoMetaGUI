b906a1871e7a6c0bde821cf4210aa18fff422682
Delano Oliveira <delanohelio@gmail.com>
2015-06-08 21:58:09 -0300

    Adding NumberFormatter

--- src/main/java/com/nanuvem/metagui/server/sample/Main.java
+++ src/main/java/com/nanuvem/metagui/server/sample/Main.java
@@ -17,7 +17,10 @@ public class Main {
 	public static void main(String[] args) {
 		MetaGuiEntryPoint.run(args);
 		Widget datePickerWidget = MetaGuiEntryPoint.createWidget("DatePickerWidget", WidgetType.Property, MetaGuiEntryPoint.readWidgetFile("DatePickerWidget.js"));
-		MetaGuiEntryPoint.createRule("field", null, PropertyTypeType.date, null, datePickerWidget, null);
+		Widget numberFormatterWidget = MetaGuiEntryPoint.createWidget("NumberFormatterWidget", WidgetType.Property, MetaGuiEntryPoint.readWidgetFile("NumberFormatter.js"));
+		MetaGuiEntryPoint.createRule("field", null, PropertyTypeType.date, null, null, datePickerWidget, null);
+		MetaGuiEntryPoint.createRule("field", null, null, "cpf", null, numberFormatterWidget, "{\"format\": \"000.000.000-00\", \"editable\": true}");
+		MetaGuiEntryPoint.createRule("property", null, null, "cpf", null, numberFormatterWidget, "{\"format\": \"000.000.000-00\", \"editable\": false}");
 		DomainModelContainer.deploy(Cliente.class, Fornecedor.class,
 				ItemPedido.class, Pedido.class, Produto.class, Vendedor.class);
 	}
