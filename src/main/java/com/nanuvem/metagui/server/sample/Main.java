59af563e4d95f114f6be8e1280c59454515b5730
Delano Oliveira <delanohelio@gmail.com>
2015-06-13 20:37:12 -0300

    Adding new rule to Fornecedor

--- src/main/java/com/nanuvem/metagui/server/sample/Main.java
+++ src/main/java/com/nanuvem/metagui/server/sample/Main.java
@@ -20,15 +20,21 @@ public class Main {
 		Widget datePickerWidget = MetaGuiEntryPoint.createWidget("DatePickerWidget", WidgetType.Property, MetaGuiEntryPoint.readWidgetFile("DatePickerWidget.js"));
 		Widget numberFormatterWidget = MetaGuiEntryPoint.createWidget("NumberFormatterWidget", WidgetType.Property, MetaGuiEntryPoint.readWidgetFile("NumberFormatter.js"));
 		Widget comboBoxWidget = MetaGuiEntryPoint.createWidget("ComboBoxWidget", WidgetType.Relationship, MetaGuiEntryPoint.readWidgetFile("ComboBoxWidget.js"));
-		MetaGuiEntryPoint.createRule("field", null, PropertyTypeType.date, null, null, datePickerWidget, null);
+		MetaGuiEntryPoint.createRule("field", null, PropertyTypeType.date.name(), null, null, datePickerWidget, null);
 		MetaGuiEntryPoint.createRule("field", null, null, "cpf", null, numberFormatterWidget, "{\"format\": \"000.000.000-00\", \"editable\": true}");
 		MetaGuiEntryPoint.createRule("property", null, null, "cpf", null, numberFormatterWidget, "{\"format\": \"000.000.000-00\", \"editable\": false}");
-		MetaGuiEntryPoint.createRule("fieldRelation", null, null, null, Cardinality.One, comboBoxWidget, "{\"propertyKey\": \"nomeFantasia\"}");
+		MetaGuiEntryPoint.createRule("fieldRelation", null, "Fornecedor", null, Cardinality.One, comboBoxWidget, "{\"propertyKey\": \"nomeFantasia\"}");
 		DomainModelContainer.deploy(Cliente.class, Fornecedor.class,
 				ItemPedido.class, Pedido.class, Produto.class, Vendedor.class);
 		Fornecedor fornecedor = new Fornecedor();
 		fornecedor.setNomeFantasia("Nanuvem");
+		Fornecedor fornecedor2 = new Fornecedor();
+		fornecedor2.setNomeFantasia("Embedded");
+		Fornecedor fornecedor3 = new Fornecedor();
+		fornecedor3.setNomeFantasia("Asus");
 		DomainModelContainer.saveInstance("fornecedores", fornecedor);
+		DomainModelContainer.saveInstance("fornecedores", fornecedor2);
+		DomainModelContainer.saveInstance("fornecedores", fornecedor3);
 	}
 
 }
